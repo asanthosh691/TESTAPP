573dec4c0b12c878725fb7a5be33047d6d22d737
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-01 14:01:31 +0530

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TESTAPP.Startup))]
+namespace TESTAPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
