6313dc539e35af87f0334c9c4a89d87e33f779d1
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-15 14:51:08 +0530

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -1,7 +1,8 @@
 ﻿Import-Module WebAdministration
+
     #create the app pool
-$appPool = New-Item 'TEST'
-$appPool | Set-ItemProperty -Name -Path 'IIS:\AppPools\Teest' -name processModel -value @{username="admin"; password="admin"; identitytype=3}
+    $appPool = New-Item 'TEST'
+    $appPool | Set-ItemProperty -Path 'IIS:\AppPools\Teest' -name processModel -value @{username="admin"; password="admin"; identitytype=3}
 
 
  
