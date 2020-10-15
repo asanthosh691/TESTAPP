41b424ec9cbae538d0c2261d6f999bc8b922d12b
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-15 12:51:09 +0530

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -3,18 +3,6 @@ $appPoolName = 'Teest'
 $WebSiteName='TEST'
 $WebAppName='Default Web Site'
 $AppFolder='C:\inetpub\wwwroot'
-if(Test-Path(IIS:\AppPools\$appPoolName))
-{        
-  Write-Host 'Web is Created with new App pool'`n
-}else{
+
 New-WebAppPool -Name $appPoolName -Force
 Set-ItemProperty -Path IIS:\AppPools\$appPoolName -name processModel -value @{username="admin"; password="admin"; identitytype=3}
-}
-if (Test-Path(IIS:\Sites\$WebSiteName\$WebAppName)){
-Write-Host "Web App $WebAppName already exists" -ForegroundColor Yellow
-return
-}
-else {
-New-Item -Name 'Engage' -ItemType directory -Path C:\inetpub\wwwroot -Force
-New-WebApplication -Name $WebSiteName -Site $WebAppName -PhysicalPath C:\inetpub\wwwroot\'Engage' -ApplicationPool $appPoolName
-}
