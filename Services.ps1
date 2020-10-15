5b94d4b08ee63a8e73c9173e1f03eda1d2b955e5
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-15 15:56:22 +0530

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -1,9 +1,1 @@
-﻿$appPoolName = 'MyAppPool'
-$scriptBlock = {
-    Import-Module WebAdministration
-    New-Item –Path IIS:\AppPools\$using:appPoolName
-    Set-ItemProperty -Path
-    IIS:\AppPools\$using:appPoolName -Name
-    managedRuntimeVersion -Value 'v4.0'    
-}
-Invoke-Command  –ScriptBlock $scriptBlock
+﻿New-Item –Path IIS:\AppPools\TEST
