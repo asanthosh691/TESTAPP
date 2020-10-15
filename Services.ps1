f38030029101eea83a2267e9bc7315974d533bee
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-15 14:45:44 +0530

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -1,12 +1,7 @@
 ﻿Import-Module WebAdministration
-cd IIS:\AppPools\
-
-#check if the app pool exists
-if (!(Test-Path IIS:\AppPools\'TEST' -pathType container))
-{
     #create the app pool
-    $appPool = New-Item 'TEST'
-    $appPool | Set-ItemProperty -Name -Path 'IIS:\AppPools\Teest' -name processModel -value @{username="admin"; password="admin"; identitytype=3}
-}
+$appPool = New-Item 'TEST'
+$appPool | Set-ItemProperty -Name -Path 'IIS:\AppPools\Teest' -name processModel -value @{username="admin"; password="admin"; identitytype=3}
+
 
  
