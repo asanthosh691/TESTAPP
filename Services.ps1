f0fd0f2ad04f31e087d8b584bce6d5be7d06b963
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-15 10:03:49 +0530

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -1,26 +1,7 @@
 ﻿Try
 {
-        Remove-Item c:\Deployment -Force -Recurse -ErrorAction SilentlyContinue
-        Expand-Archive -LiteralPath C:\cfn\ebdata\source_bundle.zip -DestinationPath c:\Deployment -ErrorAction SilentlyContinue
-        $appPoolName = 'Teest'
-        $WebSiteName='TEST'
-        $WebAppName='Default Web Site'
-        $AppFolder='C:\inetpub\wwwroot'
-        if(Test-Path("IIS:\AppPools\" + $appPoolName))
-        {        
-          Write-Host 'Web is Created with new App pool'`n
-        }else{
-        New-WebAppPool -Name $appPoolName -Force
-        Set-ItemProperty -Path IIS:\AppPools\$appPoolName -name processModel -value @{username="admin"; password="admin"; identitytype=3}
-        }
-        if (Test-Path("IIS:\Sites\$WebSiteName\$WebAppName")){
-        Write-Host "Web App $WebAppName already exists" -ForegroundColor Yellow
-        return
-        }
-        else {
-        New-Item -Name 'Engage' -ItemType directory -Path C:\inetpub\wwwroot -Force
-        New-WebApplication -Name $WebSiteName -Site $WebAppName -PhysicalPath C:\inetpub\wwwroot\'Engage' -ApplicationPool $appPoolName
-        }
+Remove-Item c:\Deployment -Force -Recurse -ErrorAction SilentlyContinue
+Expand-Archive -LiteralPath C:\cfn\ebdata\source_bundle.zip -DestinationPath c:\Deployment -ErrorAction SilentlyContinue
 
         #  function installsevice($servicename,$svc_dir,$svc_folder)
         #  {
